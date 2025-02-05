c48b37f9c59511337f67cdac35d5a2b08850f2a5
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-04 22:53:13 -0300

    feat: emtpy page

---
+++ src/app/components/empty_page/empty_page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EmptyPage = () => {
+  return (
+    <div>
+      <div>Nothing collected yet</div>
+      <div>Here you will see your collected games</div>
+    </div>
+  );
+};
+
+export default EmptyPage;
