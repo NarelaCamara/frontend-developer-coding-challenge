fc3151c48e0d556dfd97acc3c3743b1139acb245
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-07 13:45:25 -0300

    feat: view search

---
+++ src/app/components/search_view/search_view.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Categories from "../categories/categories";
+import ItemList from "../itemList/itemList";
+
+const SearchView = () => {
+  return (
+    <div className="">
+      <Categories />
+      <ItemList/>
+    </div>
+  );
+};
+
+export default SearchView;
