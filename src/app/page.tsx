40fd2862bfd0174396bfce17966a840ed643cd0d
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-07 13:45:10 -0300

    feat: pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import DescriptionView from "./components/description_view/description_view";
 import SearchView from "./components/search_view/search_view";
 
 export default function Home() {
   return (
     <div>
       <SearchView />
+      <DescriptionView />
     </div>
   );
 }
