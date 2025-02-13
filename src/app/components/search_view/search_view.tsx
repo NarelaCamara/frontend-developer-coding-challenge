3a5f7d0ccf0b3ede5f3ae745f77a14a9a181bd7f
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-12 23:24:45 -0300

    refactor: correccion nombre

--- src/app/components/search_view/search_view.tsx
+++ src/app/components/search_view/search_view.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Categories from "../categories/categories";
-import ItemList from "../itemList/itemList";
+import ItemList from "../item_list/item_list";
 
 const SearchView = () => {
   return (
