ac89054766f0695ae86a060b26339b96c1c8d132
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-20 22:20:00 -0300

    feat: change div innecesari

--- src/app/components/item/item.tsx
+++ src/app/components/item/item.tsx
@@ -5,20 +5,18 @@ import React from "react";
 
 const Item = ({ element }: { element: string }) => {
   return (
-    <div className="m-2">
-      <div className="w-52 bg-pink-400 relative rounded-xl  ">
-        <Image
-          className="dark:invert rounded-xl"
-          src={element}
-          alt="Vercel logomark"
-          width={250}
-          height={250}
-        />
-        <div className="absolute bottom-0 right-0 pb-3 pr-3">
-          <IconButton radius="full" size="4" color="gray">
-            <Trash2 size={20} />
-          </IconButton>
-        </div>
+    <div className="w-52 bg-pink-400 relative rounded-xl  m-2 ">
+      <Image
+        className="dark:invert rounded-xl"
+        src={element}
+        alt="Vercel logomark"
+        width={250}
+        height={250}
+      />
+      <div className="absolute bottom-0 right-0 pb-3 pr-3">
+        <IconButton radius="full" size="4" color="gray">
+          <Trash2 size={20} />
+        </IconButton>
       </div>
     </div>
   );
