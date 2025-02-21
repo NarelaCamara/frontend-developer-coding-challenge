801757b6058bd25ae9cecaa7941aac68b9432515
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-20 22:28:07 -0300

    feat: alinear center notification

--- src/app/components/notification/notification.tsx
+++ src/app/components/notification/notification.tsx
@@ -3,16 +3,18 @@ import { CircleCheck } from "lucide-react";
 
 const Notification = () => {
   return (
-    <Flex direction="column" gap="3" maxWidth="350px">
-      <Card variant="surface">
-        <Text as="div" size="2" weight="bold">
-          <CircleCheck color="green" /> Game collected
-        </Text>
-        <Text as="div" color="gray" size="2">
-          Grand Theft Auto V has been added to your collection
-        </Text>
-      </Card>
-    </Flex>
+    <div className="fixed inset-0 flex items-center justify-center z-20">
+      <Flex direction="column" gap="3" maxWidth="350px">
+        <Card variant="surface">
+          <Text as="div" size="2" weight="bold">
+            <CircleCheck color="green" /> Game collected
+          </Text>
+          <Text as="div" color="gray" size="2">
+            Grand Theft Auto V has been added to your collection
+          </Text>
+        </Card>
+      </Flex>
+    </div>
   );
 };
 
