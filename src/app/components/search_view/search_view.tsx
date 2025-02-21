b86fdc6779538ad1f1637ba7af8b678231922ac5
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-20 22:51:44 -0300

    feat: serch view items

--- src/app/components/search_view/search_view.tsx
+++ src/app/components/search_view/search_view.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Categories from "../categories/categories";
-import ItemList from "../item_list/item_list";
+import { Grid } from "@radix-ui/themes";
+import Item from "../item/item";
 
 const SearchView = () => {
   return (
     <div className="w-3/4 mx-auto max-w-screen-lg">
       <Categories />
-      <ItemList />
+      <Grid columns="4" gap="4" width="auto">
+        {[
+          1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5,
+          6,
+        ].map((e, i) => (
+          <Item element={"/image.png"} key={i} viewTrash={false} />
+        ))}
+      </Grid>
     </div>
   );
 };
