2445c2c1890e3fe1e3927ab9ea407bc6fcc07dbc
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-20 22:33:58 -0300

    feat: icono trash no view

--- src/app/components/description_view/description_view.tsx
+++ src/app/components/description_view/description_view.tsx
@@ -2,83 +2,79 @@ import { Button, Grid, Heading } from "@radix-ui/themes";
 import Image from "next/image";
 import React from "react";
 import Item from "../item/item";
+import Notification from "../notification/notification";
+import Genre_rating_release from "../gender_rating_release/gender_rating_release";
 
 const DescriptionView = () => {
   return (
-    <div>
-      <div>
-        <Image
-          className="dark:invert rounded-xl"
-          src={"/image.png"}
-          alt="Vercel logomark"
-          width={150}
-          height={150}
-        />
-      </div>
-      <Heading className="m-2 Heading -2xl text-purple-800">
-        Call of Duty
-      </Heading>
+    <>
+      <Notification />
 
-      <Button variant="solid" radius="full" color="violet">
-        Collect game
-      </Button>
-
-      <div className="flex flex-row justify-center content-center items-center pb-8">
-        <div className="m-1">
-          <Button radius="full" color="violet">
-            Rating
-          </Button>
-        </div>
-        <div className="m-1">
-          <Button radius="full" color="violet">
-            Release:
-          </Button>
-        </div>
-        <div className="m-1">
-          <Button radius="full" color="violet">
-            Genre:
-          </Button>
-        </div>
-      </div>
-      <div>
-        <Heading className="m-2 Heading -2xl text-purple-800">Summary</Heading>
+      <div className="w-3/4 mx-auto max-w-screen-lg">
         <div>
-          Grand Theft Auto V is a vast open world game set in Los Santos, a
-          sprawling sun-soaked metropolis struggling to stay afloat in an era of
-          economic uncertainty and cheap reality TV. The game blends
-          storytelling and gameplay in new ways as players repeatedly jump in
-          and out of the lives of the game’s three lead characters, playing all
-          sides of the game’s interwoven story.
+          <Image
+            className="dark:invert rounded-xl"
+            src={"/image.png"}
+            alt="Vercel logomark"
+            width={150}
+            height={150}
+          />
+        </div>
+        <Heading className="m-2 Heading -2xl text-purple-800">
+          Call of Duty
+        </Heading>
+
+        <Button variant="solid" radius="full" color="violet">
+          Collect game
+        </Button>
+
+        <div>
+          <Genre_rating_release
+            rating={"9.9"}
+            release={"12/01/20"}
+            gender={"Card & Board Game"}
+          />
+          <Heading className="m-2 Heading -2xl text-purple-800">
+            Summary
+          </Heading>
+          <div>
+            Grand Theft Auto V is a vast open world game set in Los Santos, a
+            sprawling sun-soaked metropolis struggling to stay afloat in an era
+            of economic uncertainty and cheap reality TV. The game blends
+            storytelling and gameplay in new ways as players repeatedly jump in
+            and out of the lives of the game’s three lead characters, playing
+            all sides of the game’s interwoven story.
+          </div>
+        </div>
+
+        <div>
+          <Heading className="m-2 Heading -2xl text-purple-800">
+            Platforms
+          </Heading>
+          <div>PC (Microsoft Windows)</div>
+        </div>
+
+        <div>
+          <Heading className="m-2 Heading -2xl text-purple-800">Media</Heading>
+          <Grid columns="4" gap="4" width="auto">
+            {[1, 2, 3, 4, 5, 6].map((e) => (
+              <Item element={"/image.png"} key={e} />
+            ))}
+          </Grid>
+        </div>
+
+        <div>
+          <Heading className="m-2 Heading -2xl text-purple-800">
+            Similar games
+          </Heading>
+          <Grid columns="4" gap="4" width="auto">
+            {[1, 2, 3, 4, 5, 6].map((e) => (
+              <Item element={"/image.png"} key={e} viewTrash={false} />
+            ))}
+          </Grid>
         </div>
       </div>
-
-      <div>
-        <Heading className="m-2 Heading -2xl text-purple-800">
-          Platforms
-        </Heading>
-        <div>PC (Microsoft Windows)</div>
-      </div>
-
-      <div>
-        <Heading className="m-2 Heading -2xl text-purple-800">Media</Heading>
-        <Grid columns="4" gap="4" width="auto">
-          {[1, 2, 3, 4, 5, 6].map((e) => (
-            <Item element={"/image.png"} key={e} />
-          ))}
-        </Grid>
-      </div>
-
-      <div>
-        <Heading className="m-2 Heading -2xl text-purple-800">
-          Similar games
-        </Heading>
-        <Grid columns="4" gap="4" width="auto">
-          {[1, 2, 3, 4, 5, 6].map((e) => (
-            <Item element={"/image.png"} key={e} />
-          ))}
-        </Grid>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/app/components/item/item.tsx
+++ src/app/components/item/item.tsx
@@ -3,7 +3,13 @@ import { Trash2 } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-const Item = ({ element }: { element: string }) => {
+const Item = ({
+  element,
+  viewTrash,
+}: {
+  element: string;
+  viewTrash?: boolean;
+}) => {
   return (
     <div className="w-52 bg-pink-400 relative rounded-xl  m-2 ">
       <Image
@@ -13,11 +19,13 @@ const Item = ({ element }: { element: string }) => {
         width={250}
         height={250}
       />
-      <div className="absolute bottom-0 right-0 pb-3 pr-3">
-        <IconButton radius="full" size="4" color="gray">
-          <Trash2 size={20} />
-        </IconButton>
-      </div>
+      {viewTrash && (
+        <div className="absolute bottom-0 right-0 pb-3 pr-3">
+          <IconButton radius="full" size="4" color="gray">
+            <Trash2 size={20} />
+          </IconButton>
+        </div>
+      )}
     </div>
   );
 };
