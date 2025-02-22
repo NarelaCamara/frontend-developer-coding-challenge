538377e76860947f9051800e37abbc39bb965c95
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-21 22:56:58 -0300

    refactor: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import DescriptionView from "./components/description_view/description_view";
 import SearchView from "./components/search_view/search_view";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <SearchView />
-      <DescriptionView />
-    </div>
+    </>
   );
 }
