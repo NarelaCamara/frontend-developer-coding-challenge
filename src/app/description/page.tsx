fda29de8e45c2450d384b9fe7ed2540700016a4c
RoxanaCamara <roxana.camara07@gmail.com>
2025-02-21 22:56:04 -0300

    feat: ruta description

---
+++ src/app/description/page.tsx
@@ -0,0 +1,6 @@
+import DescriptionView from "@/app/components/description_view/description_view";
+import React from "react";
+
+export default function Description() {
+  return <DescriptionView />;
+}
