58fa986a3f75b21da8bfdaeb618592fa71c1778c
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-01 16:56:12 -0300

    feat: agregamos proxy, eliminamos error de cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/igdb/:path*",
+        destination: "https://api.igdb.com/v4/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/features/games/infra/GamesRepository.ts
+++ src/features/games/infra/GamesRepository.ts
@@ -4,21 +4,17 @@ import { IGamesResult } from "../domain/IGamesResult";
 
 export function GamesRepository(): IGamesRepository {
   const gamesInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    baseURL: "/api/igdb",
     timeout: 1000,
   });
 
   const getGames = async (accessToken: string): Promise<IGamesResult> => {
-    const { data } = await gamesInstance.post(
-      `/games`,
-      { query: "fields *;" },
-      {
-        headers: {
-          "Client-ID": process.env.NEXT_PUBLIC_CLIENTE_ID,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const { data } = await gamesInstance.post(`/games`, "fields *;", {
+      headers: {
+        "Client-ID": process.env.NEXT_PUBLIC_CLIENTE_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     return data;
   };
 
