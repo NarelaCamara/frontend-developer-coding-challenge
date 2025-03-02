06c0f119fc322901c6f759b9be281421aaffb29f
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-02 18:44:05 -0300

    feat: add exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Categories } from "./categories/categories";
+export { default as Item } from "./item/item";
 export { default as DescriptionView } from "@/components/description_view/description_view";
-export { default as SearchView } from "@/components/search_view/search_view";
 export { default as Navigation } from "@/components/navigation/navigation";
--- src/features/index.ts
+++ src/features/index.ts
@@ -1,2 +1,3 @@
+export type { IGame } from "./games/domain/IGame";
 export { getGames } from "./games/aplication/getGames";
 export { login } from "./auth/aplication/login";
