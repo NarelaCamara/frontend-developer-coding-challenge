5ceb7a07f1f3a6901e9fb2fc3845c0acfdebf839
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-02 18:25:02 -0300

    feat: iamges domain

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.igdb.com"], // Add IGDB images domain here
+  },
   async rewrites() {
     return [
       {
