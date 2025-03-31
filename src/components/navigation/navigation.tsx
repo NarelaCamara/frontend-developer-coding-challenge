a5d4c47d6f3f7e2e9d9166b87f799278470fcda5
RoxanaCamara <roxana.camara07@gmail.com>
2025-03-31 18:22:03 -0300

    refactor: navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -9,33 +9,33 @@ const Navigation = () => {
   const router = useRouter();
   const path = usePathname();
 
-  console.log(router, path);
   return (
     <div>
-      <div className="grid grid-rows-1 items-center justify-items-center">
-        <div className="flex flex-row items-center justify-items-center m-2">
-          <div className=" rounded-xl border-2 border-pink-400 p-2">
-            <SwordsIcon size={23} color="#3C1661" />
-          </div>
+      {path === "/" && (
+        <div className="grid grid-rows-1 items-center justify-items-center">
+          <div className="flex flex-row items-center justify-items-center m-2">
+            <div className=" rounded-xl border-2 border-pink-400 p-2">
+              <SwordsIcon size={23} color="#3C1661" />
+            </div>
 
-          {
             <Heading className="m-2 Heading -2xl text-purple-800">
               Gaming Haven Z
             </Heading>
-          }
+          </div>
         </div>
-      </div>
-      {path !== "/" && (
-        <div>
+      )}
+
+      <div className="flex flex-row justify-between w-2/3">
+        {path !== "/" && (
           <h2
             onClick={() => router.back()}
             className="text-base p-4 font-bold text-purple-800 flex flex-row items-center"
           >
             <ArrowLeft height="20" width="20" /> Back
           </h2>
-          <Search />
-        </div>
-      )}
+        )}
+        <Search />
+      </div>
     </div>
   );
 };
